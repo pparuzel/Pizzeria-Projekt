7aa7b8f7435f3976f92d83dac56544ff4dbd972d
Piotr Persona <persona.piotr@gmail.com>
2017-11-15 13:19:38 +0100

    Dodaj zawartość app.py i settings.py

--- python/github/app.py
+++ python/github/app.py
@@ -1,20 +1,8 @@
-import os
+from flask import Flask  # external
+from flask_flatpages import FlatPages  # external
+from flask_frozen import Freezer  # external
 
-REPO_NAME = "Pizzeria-Projekt"
-DEBUG = True
-
-APP_DIR = os.path.dirname(os.path.abspath(__file__))
-
-def parent_dir(path):
-    '''Return the parent of a directory.'''
-    return os.path.abspath(os.path.join(path, os.pardir))
-
-PROJECT_ROOT = parent_dir(APP_DIR)
-
-FREEZER_DESTINATION = PROJECT_ROOT
-FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
-FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
-                                    # will be deleted when you run the freezer
-FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
-FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
-FLATPAGES_EXTENSION = '.md'
+app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+pages = FlatPages(app)
+freezer = Freezer(app)
