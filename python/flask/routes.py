108ee4e17abedca40482a0fe13cc09b88d82e8d3
piotrpersona <persona.piotr@gmail.com>
2017-12-11 20:59:08 +0100

    Dodać endpointy dla gościa.

--- python/flask/routes.py
+++ python/flask/routes.py
@@ -1,7 +1,43 @@
-from flask import redirect, render_template, url_for
+from flask import redirect, render_template, url_for, request
 
 from python.flask.app import app
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        print('† index')
+        return render_template('index.html')
+
+
+@app.route('/profile', methods=['GET'])
+def user_profile():
+    if request.method == 'GET':
+        return render_template('profile.html')
+
+
+@app.route('/menu', methods=['GET'])
+def show_menu():
+    if request.method == 'GET':
+        return render_template('menu.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        print('login')
+        return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        print('register')
+        return redirect(url_for('index'))
+
+
+@app.route('/update_user', methods=['POST'])
+def update_user():
+    if request.method == 'POST':
+        print('user update')
+        return redirect(url_for('user_profile'))
