76418e077ded1df751f9570b534a6f48d35ad325
piotrpersona <persona.piotr@gmail.com>
2017-12-12 19:58:10 +0100

    Dodaj nawigację po menu. Dodaj podstawowe szablony.

--- python/flask/routes.py
+++ python/flask/routes.py
@@ -2,12 +2,16 @@ from flask import redirect, render_template, url_for, request
 
 from python.flask.app import app
 
+#
+# guest
+#
 
 @app.route('/', methods=['GET'])
 def index():
     if request.method == 'GET':
         print('† index')
-        return render_template('index.html')
+        is_logged_in = True
+        return render_template('index.html', logged_in=is_logged_in)
 
 
 # get user's info from database
@@ -15,12 +19,6 @@ def index():
 def user_profile():
     if request.method == 'GET':
         return render_template('profile.html')
-
-
-@app.route('/menu', methods=['GET'])
-def show_menu():
-    if request.method == 'GET':
-        return render_template('menu.html')
 
 
 # validate user's info from database
@@ -39,10 +37,59 @@ def register():
         return redirect(url_for('index'))
 
 
+#
+# menu
+#
+
+@app.route('/menu', methods=['GET'])
+def show_menu():
+    # read from DB
+    pizzas = [
+        ['margarita', 'sos, ser, oregano, bazylia', 21.50],
+        ['salami', 'sos, ser, oregano, salami', 22.50],
+        ['z szynką', 'sos, ser, oregano, szynka', 22.50],
+        ['hawajska', 'sos, ser, oregano, ananas, szynka', 24.50]
+    ]
+    if request.method == 'GET':
+        is_logged_in = True
+        return render_template('menu.html', products=pizzas, logged_in=is_logged_in)
+
+
+@app.route('/show_order', methods=['POST', 'GET'])
+def show_current_order():
+    import random
+    if request.method == 'POST':
+        selected_products = request.form.getlist('check_group')
+        if len(selected_products) > 0:
+            print(selected_products)
+            delivery_time = random.randint(45,90)
+            return render_template('order_page.html',
+                                   selected_products=selected_products,
+                                   delivery_time=delivery_time)
+        else:
+            return redirect(url_for('show_menu'))
+    elif request.method == 'GET':
+        return render_template('order_page.html')
+
+
+@app.route('/make_order', methods=['POST', 'GET'])
+def make_order():
+    # put order into DB
+    # show pop up
+    return redirect(url_for('index'))
+
+
+#
+# user
+#
+
 # update user with database
 @app.route('/update_user', methods=['POST'])
 def update_user():
     if request.method == 'POST':
         print('user update')
         return redirect(url_for('user_profile'))
+
+
+
 
